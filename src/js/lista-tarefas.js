51219b244f085817adce703aa7216ad2ae9b5a9e
Gabriel Suldine <82013387+Gabriel-Suldine@users.noreply.github.com>
2022-11-03 22:06:41 -0400

    ✨ Funcionalidade:  Remover tarefa

--- src/js/lista-tarefas.js
+++ src/js/lista-tarefas.js
@@ -1,4 +1,37 @@
 import './nav.js'
 import '../css/lista-tarefas.css'
 
+const inputNovaTarefa = document.querySelector('#input-nova-tarefa');
+const btnAdicionar = document.querySelector('#btn-adicionar');
+const listaTarefas = document.querySelector('#lista-tarefas');
+
+btnAdicionar.addEventListener('click', () => {
+    let descricaoTarefa = inputNovaTarefa.value
+    const li = document.createElement("li");
+    li.className = 'tarefa';
+    li.innerHTML = `
+    <div>${descricaoTarefa} </div>
+    
+       `;
+    listaTarefas.appendChild(li);
+    //criar elemento
+    let btnExcluir = document.createElement('button');
+    btnExcluir.setAttribute('id', 'btn--Excluir');
+    btnExcluir.className = 'btn-excluir';
+    btnExcluir.textContent = 'Excluir';
+
+
+    li.appendChild(btnExcluir);
+
+    btnExcluir.addEventListener('click', () => {
+        let tarefa = btnExcluir.parentElement
+        listaTarefas.removeChild(tarefa)
+
+
+    })
+
+})
+
+
+
 
